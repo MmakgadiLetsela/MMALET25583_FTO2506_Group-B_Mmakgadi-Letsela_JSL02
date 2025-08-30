b75d568ff0a1f2dfe7af99f58bf09d8c6460e43b
MmakgadiLetsela <mmakgadi.letsela@gmail.com>
2025-08-30 22:03:53 +0200

    add alert for invalid task status

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 // TASK ONE
-let title =prompt("Enter task 1 title:")
-console.log(title);
+let title_1 =prompt("Enter task 1 title:")
+console.log(title_1);
 
-let desc =prompt("Enter task 1 description:")
-console.log(desc);
+let desc_1 =prompt("Enter task 1 description:")
+console.log(desc_1);
 
-let status = ["todo", "doing", "done"];
+const validStatus =["todo", "doing", "done"];
+let userStatus_1 =prompt("Enter task 1 status (todo, doing, done):")
+while (userStatus_1 !== validStatus) {
+userStatus_1 =alert("Invalid status. Please enter 'todo', 'doing', or 'done'.");
+}
+if (userStatus_1 === validStatus) {
+    
+console.log(userStatus_1);
 
