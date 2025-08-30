1fb6d897b36897a325e05c4d4565645440d54893
MmakgadiLetsela <mmakgadi.letsela@gmail.com>
2025-08-30 21:22:22 +0200

    added title and desc prompt

---
+++ script.js
@@ -0,0 +1,9 @@
+// TASK ONE
+let title =prompt("Enter task 1 title:")
+console.log(title);
+
+let desc =prompt("Enter task 1 description:")
+console.log(desc);
+
+let status = ["todo", "doing", "done"];
+
