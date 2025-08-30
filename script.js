20ca51b3a1e599a440dad4c905116e5e48147c04
MmakgadiLetsela <mmakgadi.letsela@gmail.com>
2025-08-30 22:17:17 +0200

    add task 2 status alert and prompt

--- script.js
+++ script.js
@@ -21,3 +21,10 @@ console.log(title_2);
 let desc_2 =prompt("Enter task 2 description:")
 console.log(desc_2);
 
+let userStatus_2 =prompt("Enter task 2 status (todo, doing, done):")
+while (userStatus_2 !== validStatus) {
+userStatus_2 =alert("Invalid status. Please enter 'todo', 'doing', or 'done'.");
+}
+if (userStatus_2 === validStatus) {
+}
+console.log(userStatus_2);
