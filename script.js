db0f93ecf19305ce34ac92b9e5efc06ae6630cf7
MmakgadiLetsela <mmakgadi.letsela@gmail.com>
2025-08-30 22:08:42 +0200

    add task 2 titile and desc prompts

--- script.js
+++ script.js
@@ -11,6 +11,13 @@ while (userStatus_1 !== validStatus) {
 userStatus_1 =alert("Invalid status. Please enter 'todo', 'doing', or 'done'.");
 }
 if (userStatus_1 === validStatus) {
-    
+}
 console.log(userStatus_1);
+
+// TASK TWO
+let title_2 =prompt("Enter task 2 title:")
+console.log(title_2);
+
+let desc_2 =prompt("Enter task 2 description:")
+console.log(desc_2);
 
