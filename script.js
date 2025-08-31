518fd13c9a19b87724573c6bd545d3071acbf05d
MmakgadiLetsela <mmakgadi.letsela@gmail.com>
2025-08-31 12:15:36 +0200

    add comments

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ let task_1 = {
     title_1: prompt("Enter task 1 title:"),
     desc_1: prompt("Enter task 1 description:"),
     status_1: prompt("Enter task 1 status (todo, doing, done):").toLowerCase(),
-};
+}; // Task 1 object with user inputs
+
 
 while (status_1 !== "todo" && status_1 !== "doing" && status_1 !== "done") {
     alert("Invalid status. Please enter todo, doing, or done.");
     task_1.status_1 =prompt("Enter task 1 status (todo, doing, done):").toLowerCase();
-}
+} // Valid status check loop
+
 console.log(task_1);
 
 
@@ -19,17 +21,22 @@ let task_2 = {
     title_2: prompt("Enter task 2 title:"),
     desc_2: prompt("Enter task 2 description:"),
     status_2: prompt("Enter task 2 status (todo, doing, done):").toLowerCase(),
-};
+    
+}; // Task 2 object with user inputs
+
 while (status_2 !== "todo" && status_2 !== "doing" && status_2 !== "done") {
     alert("Invalid status. Please enter todo, doing, or done.");
     task_2.status_2 = prompt("Enter task 2 status (todo, doing, done):").toLowerCase();
-}
+} // Valid status check loop
+
 console.log(task_2);
 
-
+// Conditional alert
 if (task_1.status_1 !== "done" && task_2.status_2 !== "done") {
-    alert("No tasks completed, let's get to work.");
+     alert("No tasks completed, let's get to work."); 
 }
+
+// Conditional logging 
 if (task_1.status_1 === "done") {
     console.log(`Title: ${task_1.status_1}, Status: ${task_1.status_1}`);
 }
