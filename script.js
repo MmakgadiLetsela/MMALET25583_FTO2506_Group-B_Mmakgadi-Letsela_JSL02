b6cb3d7d17955a4d229b37c6c4f826e0104ff24f
MmakgadiLetsela <mmakgadi.letsela@gmail.com>
2025-08-31 11:56:40 +0200

    add conditional log

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let task_1 = {
 
 while (status_1 !== "todo" && status_1 !== "doing" && status_1 !== "done") {
     alert("Invalid status. Please enter todo, doing, or done.");
-    status_1 =prompt("Enter task 1 status (todo, doing, done):").toLowerCase();
+    task_1.status_1 =prompt("Enter task 1 status (todo, doing, done):").toLowerCase();
 }
 console.log(task_1);
 
@@ -22,13 +22,17 @@ let task_2 = {
 };
 while (status_2 !== "todo" && status_2 !== "doing" && status_2 !== "done") {
     alert("Invalid status. Please enter todo, doing, or done.");
-    status_2 = prompt("Enter task 2 status (todo, doing, done):").toLowerCase();
+    task_2.status_2 = prompt("Enter task 2 status (todo, doing, done):").toLowerCase();
 }
 console.log(task_2);
 
 
-if (status_1 !== done && status_2 !== done) {
+if (task_1.status_1 !== "done" && task_2.status_2 !== "done") {
     alert("No tasks completed, let's get to work.");
 }
-if (status_1 === done || status_2 === done) {
-    console.log(Title: "title_1")
+if (task_1.status_1 === "done") {
+    console.log(`Title: ${task_1.status_1}, Status: ${task_1.status_1}`);
+}
+if (task_2.status_2 === "done") {
+    console.log(`Title: ${task_2.status_2} Status: ${task_2.status_2}`);
+}
