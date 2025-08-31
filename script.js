ea4c19ea443af280fdaea304768861bdd4e7445d
MmakgadiLetsela <mmakgadi.letsela@gmail.com>
2025-08-31 11:19:20 +0200

    fix status alert

--- script.js
+++ script.js
@@ -8,12 +8,26 @@ console.log(desc_1);
 
 let status_1 =prompt("Enter task 1 status (todo, doing, done):").toLowerCase();
 
-while (status_1 !== "todo" || status_1 !== "doing" || status_1 !== "done") {
-status_1 =alert("Invalid status. Please enter 'todo', 'doing', or 'done'.");
+while (status_1 !== "todo" && status_1 !== "doing" && status_1 !== "done") {
+    alert("Invalid status. Please enter todo, doing, or done.");
+    status_1 =prompt("Enter task 1 status (todo, doing, done):").toLowerCase();
 }
-status_1 =prompt("Enter task 1 status (todo, doing, done):").toLowerCase();
-
 console.log(status_1);
 console.log(`Title: ${title_1}, Status: ${status_1}`);
 
 // TASK TWO
+let title_2 =prompt("Enter task 2 title:")
+console.log(title_2);
+
+let desc_2 =prompt("Enter task 2 description:")
+console.log(desc_2);
+
+let status_2 =prompt("Enter task 2 status (todo, doing, done):").toLowerCase();
+
+while (status_2 !== "todo" || status_2 !== "doing" || status_2 !== "done") {
+    alert("Invalid status. Please enter 'todo', 'doing', or 'done'.");
+    status_2 =prompt("Enter task 2 status (todo, doing, done):").toLowerCase();
+}
+
+console.log(status_2);
+console.log(`Title: ${title_2}, Status: ${status_2}`);
